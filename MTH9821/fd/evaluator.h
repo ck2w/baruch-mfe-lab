11efb1881d3cfc05b06c83a69494934cbc6466cd
quan <sqzhou2004@gmail.com>
2016-11-16 22:00:15 -0500

    Added Evaluator class

---
+++ MTH9821/fd/evaluator.h
@@ -0,0 +1,28 @@
+#ifndef EVALUATOR_H
+#define EVALUATOR_H 
+
+class Evaluator
+{
+    public:
+
+        Evaluator() {}
+        virtual double operator()(double s) const=0;
+};
+
+class FunctionEvaluator : public Evaluator
+{
+    public:
+
+        FunctionEvaluator(double (*f)(double)) : d_f(f) {}
+
+        virtual double operator()(double s) const
+        {
+            return (*d_f)(s);
+        }
+        
+    private:
+
+        double (*d_f)(double);
+};
+
+#endif /* EVALUATOR_H */
